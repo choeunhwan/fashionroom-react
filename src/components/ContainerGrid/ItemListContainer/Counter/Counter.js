86039a4751837e20db4ef380812e592429c7a98c
Pablo <eunhwan.pb@gmail.com>
2021-06-22 00:39:19 -0300

    Clase-5 Correccion 2

--- src/components/ContainerGrid/ItemListContainer/Counter/Counter.js
+++ src/components/ContainerGrid/ItemListContainer/Counter/Counter.js
@@ -1,62 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
 import './Counter.css';
-class Counter extends Component {
 
-    constructor() {
-        super();
+const Counter = () => {
 
-        this.state = {
-            count: 1,
-        };
-    }
+    const [count, setCount] = React.useState(1);
+    const [message, setMessage] = React.useState("");
 
-    handleCounterUp = () => {
-        this.setState({ count: this.state.count + 1 });
-        if (this.state.count <= 10) {
-            console.log ('Agregando 1');
+    const handleCounterUp = () => {
+        if (count < 10) {
+          setCount(count + 1);
+          setMessage(null);
         } else {
-            console.log ('Hasta 10 productos')
+          setMessage("Limite de stock 10");
         }
-    };
-
-    handleCounterDown = () => {
-        this.setState({ count: this.state.count - 1 });
-        if (this.state.count > 1) {
-            console.log ('Restando 1');
+      }
+    
+      const handleCounterDown = () => {
+        if (count > 1) {
+          setCount(count - 1);
+          setMessage(null);
         } else {
-            console.log ('No puedes menos de 1')
+          setMessage("Minima compra 1");
         }
-    };
+      }
+    
 
-
-    render() {
-        return (
+    return (
             <div>
                 <Container>
                     <Row className="justify-content-md-center">
                         <Col md="0">
-                            <button className='buttonStyle-plus' primary onClick={this.handleCounterUp}>
+                            <button className='buttonStyle-plus' primary onClick={handleCounterUp}>
                                 +
                             </button>
                         </Col>
 
                         <Col md="0">
                             <div className='inputStyle'>
-                                <span>{this.state.count}</span>
+                                <span>{count}</span>
                             </div>
                         </Col>
 
                         <Col md="0">
-                            <button className='buttonStyle-minus' secondary onClick={this.handleCounterDown}>
+                            <button className='buttonStyle-minus' secondary onClick={handleCounterDown}>
                                 -
                             </button>
                         </Col>
                     </Row>
                 </Container>
+                <p>{message}</p>
             </div>
         )
-    }
 }
 
 export default Counter;
